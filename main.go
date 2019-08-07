553a05a13380b2c0e95af94b05b29fdf7ee8ac5a
Matheus Almeida <mat.almeida@live.com>
2019-08-07 11:59:01 -0300

    feat(main): receive atk response from user

--- main.go
+++ main.go
@@ -4,40 +4,79 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
+	"github.com/matalmeida/shipbattle/item"
+
+	"github.com/matalmeida/shipbattle/game"
+
 	"github.com/fatih/color"
+	"github.com/matalmeida/shipbattle/field"
 )
 
 const PROMPT = ">> "
 const TENTATIVAS = 3
+const GAME_SIZE = 10
+
+type Jogo struct {
+	Campo        *field.Campo
+	CampoInimigo *field.Campo
+}
+
+func readString() string {
+	reader := bufio.NewReader(os.Stdin)
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput, "\n", "", 1)
+
+	return userInput
+}
 
 func main() {
-	// g := game.Novo()
-	reader := bufio.NewReader(os.Stdin)
-	p := color.New(color.FgBlack).Add(color.BgGreen).SprintfFunc()
+	g := game.Novo()
+	v := color.New(color.FgBlack).Add(color.BgGreen).SprintfFunc()
+	r := color.New(color.FgBlack).Add(color.BgRed).SprintfFunc()
 
-	menu := p(" 0 ") + " Sair\n" +
-		p(" 1 ") + " Atacar\n" +
-		p(" 2 ") + " Receber Ataque\n"
+	menu := v(" 0 ") + " Sair\n" +
+		v(" 1 ") + " Atacar\n" +
+		v(" 2 ") + " Receber Ataque\n"
 
-	fmt.Printf("%s\n", p("Bem vindo a Batalha Naval!"))
+	atkMenu := r(" 0 ") + " Água\n" +
+		r(" 1 ") + " Submarino\n" +
+		r(" 2 ") + " Destroyer\n" +
+		r(" 3 ") + " Hidro-Avião\n" +
+		r(" 4 ") + " Cruzador\n" +
+		r(" 5 ") + " Porta-Avião\n"
+
+	fmt.Printf("%s\n", v("Bem vindo a Batalha Naval!"))
 
 	for {
 		fmt.Printf("\n%s", menu)
 		fmt.Printf("\n%s", PROMPT)
-		userInput, _ := reader.ReadString('\n')
-		userInput = strings.Replace(userInput, "\n", "", 1)
+		userInput := readString()
 
 		switch userInput {
 		case "0":
 			fmt.Printf("\nAté a próxima!\n")
 			os.Exit(0)
 			break
+		case "1":
+			fmt.Printf("Atacando (%d, %d)\n", 1, 1)
+			fmt.Printf("\nQual foi o resultado do ataque? \n")
+			fmt.Printf("%s\n", atkMenu)
+			fmt.Printf(PROMPT)
+			atkResponse := readString()
+			tipoNave, _ := strconv.ParseInt(atkResponse, 10, 32)
+			ganhou := g.RetornoDeAtaque(1, 1, item.Nave(tipoNave))
+			if ganhou {
+				fmt.Printf("%s\n", v("Ganhei o Jogo!"))
+				os.Exit(0)
+			} else {
+				fmt.Printf("%s\n", r("Mais alguns ataques para ganhar!"))
+			}
+			break
 		default:
 			break
 		}
-
-		fmt.Printf(userInput)
 	}
 }
