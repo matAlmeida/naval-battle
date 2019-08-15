33db7cc6ec0783716ad5c0c880780e263d3fa61b
Matheus Almeida <mat.almeida@live.com>
2019-08-15 14:48:11 -0300

    refac(field): remove check on item added

--- field/field.go
+++ field/field.go
@@ -3,8 +3,6 @@ package field
 import (
 	"errors"
 
-	"github.com/jinzhu/copier"
-
 	"github.com/matalmeida/shipbattle/item"
 )
 
@@ -42,7 +40,7 @@ func (c *Campo) String() string {
 
 	for i := 0; i < c.Tamanho; i++ {
 		for j := 0; j < c.Tamanho; j++ {
-			ret += c.itens[i][j].String() + "\t"
+			ret += c.itens[i][j].String() + " "
 		}
 		ret += "\n"
 	}
@@ -54,7 +52,12 @@ func (c *Campo) GetItem(x int, y int) (*item.Item, error) {
 	if (x >= 0 && x < c.Tamanho) && (y >= 0 && y < c.Tamanho) {
 		return c.itens[x][y], nil
 	}
+	// _, file, no, ok := runtime.Caller(1)
+	// if ok {
+	// 	fmt.Printf("called from %s#%d\n", file, no)
+	// }
 
+	// fmt.Printf("\nCoord fora da matriz\n")
 	return item.Novo("nil", item.Vazio), errors.New("coordenadas fora da matriz")
 }
 
@@ -87,59 +90,15 @@ func (c *Campo) Atacar(x int, y int) (bool, *item.Item) {
 
 }
 
-func (c *Campo) checaAdjacentes(x int, y int, i *item.Item) (bool, error) {
-	i, err := c.GetItem(x, y)
-	if err != nil {
-		return false, errors.New("item não existe")
-	}
-	if i.Tipo != item.Vazio {
-		return false, errors.New("já tem um item")
-	}
-
-	upleft, _ := c.GetItem(x-1, y-1)
-	up, _ := c.GetItem(x, y-1)
-	upright, _ := c.GetItem(x+1, y-1)
-	left, _ := c.GetItem(x-1, y)
-	right, _ := c.GetItem(x+1, y)
-	downleft, _ := c.GetItem(x-1, y+1)
-	down, _ := c.GetItem(x, y+1)
-	downright, _ := c.GetItem(x+1, y+1)
-
-	if upleft.Tipo == item.Vazio &&
-		up.Tipo == item.Vazio &&
-		upright.Tipo == item.Vazio &&
-		left.Tipo == item.Vazio &&
-		right.Tipo == item.Vazio &&
-		downleft.Tipo == item.Vazio &&
-		down.Tipo == item.Vazio &&
-		downright.Tipo == item.Vazio {
-		return true, nil
-	} else {
-		if upleft.Id != i.Id &&
-			up.Id != i.Id &&
-			upright.Id != i.Id &&
-			left.Id != i.Id &&
-			right.Id != i.Id &&
-			downleft.Id != i.Id &&
-			down.Id != i.Id &&
-			downright.Id != i.Id {
-			return false, errors.New("existe alguma nave adjacente")
-		}
-
-		return true, nil
-	}
-}
-
 func (c *Campo) ColocaItem(x int, y int, id string, tipo item.Nave, direcao ...Direcao) bool {
-	i := item.Novo(id, tipo)
-	podeColocar, _ := c.checaAdjacentes(x, y, i)
-	if !podeColocar {
-		return false
+	if id == "enemy" {
+		_, eItemErro := c.GetItem(x, y)
+		if eItemErro == nil {
+			c.itens[x][y] = item.Novo(id, tipo)
+		}
+		return true
 	}
 
-	backup := Novo(10)
-	copier.Copy(&backup, &c)
-	canPlace := true
 	dir := Direita
 
 	if len(direcao) > 0 {
@@ -147,152 +106,42 @@ func (c *Campo) ColocaItem(x int, y int, id string, tipo item.Nave, direcao ...D
 	}
 
 	if tipo == item.Hidroaviao {
-		backup.itens[x][y] = i
+		c.itens[x][y] = item.Novo(id, tipo)
 		switch dir {
 		case Cima:
-			podeColocar, _ = backup.checaAdjacentes(x-1, y-1, i)
-			if !podeColocar {
-				canPlace = canPlace && false
-			} else {
-				backup.itens[x-1][y-1] = i
-
-				podeColocar, _ = backup.checaAdjacentes(x-1, y+1, i)
-				if !podeColocar {
-					canPlace = canPlace && false
-				} else {
-					backup.itens[x-1][y+1] = i
-				}
-			}
+			c.itens[x-1][y-1] = item.Novo(id, tipo)
+			c.itens[x-1][y+1] = item.Novo(id, tipo)
 			break
 		case Baixo:
-			podeColocar, _ = backup.checaAdjacentes(x+1, y-1, i)
-			if !podeColocar {
-				canPlace = canPlace && false
-			} else {
-				backup.itens[x+1][y-1] = i
-
-				podeColocar, _ = backup.checaAdjacentes(x+1, y+1, i)
-				if !podeColocar {
-					canPlace = canPlace && false
-				} else {
-					backup.itens[x+1][y+1] = i
-				}
-			}
+			c.itens[x+1][y-1] = item.Novo(id, tipo)
+			c.itens[x+1][y+1] = item.Novo(id, tipo)
 			break
 		case Esquerda:
-			podeColocar, _ = backup.checaAdjacentes(x-1, y-1, i)
-			if !podeColocar {
-				canPlace = canPlace && false
-			} else {
-				backup.itens[x-1][y-1] = i
-
-				podeColocar, _ = backup.checaAdjacentes(x+1, y-1, i)
-				if !podeColocar {
-					canPlace = canPlace && false
-				} else {
-					backup.itens[x+1][y-1] = i
-				}
-			}
+			c.itens[x-1][y-1] = item.Novo(id, tipo)
+			c.itens[x+1][y-1] = item.Novo(id, tipo)
 			break
 		default:
-			podeColocar, _ = backup.checaAdjacentes(x-1, y+1, i)
-			if !podeColocar {
-				canPlace = canPlace && false
-			} else {
-				backup.itens[x-1][y+1] = i
-
-				podeColocar, _ = backup.checaAdjacentes(x+1, y+1, i)
-				if !podeColocar {
-					canPlace = canPlace && false
-				} else {
-					backup.itens[x+1][y+1] = i
-				}
-			}
+			c.itens[x-1][y+1] = item.Novo(id, tipo)
+			c.itens[x+1][y+1] = item.Novo(id, tipo)
 			break
-		}
-
-		if canPlace {
-			c.itens[x][y] = i
-			switch dir {
-			case Cima:
-				c.itens[x-1][y-1] = i
-				c.itens[x-1][y+1] = i
-				break
-			case Baixo:
-				c.itens[x+1][y-1] = i
-				c.itens[x+1][y+1] = i
-				break
-			case Esquerda:
-				c.itens[x-1][y-1] = i
-				c.itens[x+1][y-1] = i
-				break
-			default:
-				c.itens[x-1][y+1] = i
-				c.itens[x+1][y+1] = i
-				break
-			}
-		} else {
-			return false
 		}
 	} else if tipo != item.Vazio {
 		for j := 0; j < int(tipo); j++ {
 			switch dir {
 			case Cima:
-				podeColocar, _ = backup.checaAdjacentes(x-j, y, i)
-				if !podeColocar {
-					canPlace = canPlace && false
-					break
-				}
-				backup.itens[x-j][y] = i
+				c.itens[x-j][y] = item.Novo(id, tipo)
 				break
 			case Baixo:
-				podeColocar, _ = backup.checaAdjacentes(x+j, y, i)
-				if !podeColocar {
-					canPlace = canPlace && false
-					break
-				}
-				backup.itens[x+j][y] = i
+				c.itens[x+j][y] = item.Novo(id, tipo)
 				break
 			case Esquerda:
-				podeColocar, _ = backup.checaAdjacentes(x, y-j, i)
-				if !podeColocar {
-					canPlace = canPlace && false
-					break
-				}
-				backup.itens[x][y-j] = i
+				c.itens[x][y-j] = item.Novo(id, tipo)
 				break
 			default:
-				podeColocar, _ = backup.checaAdjacentes(x, y+j, i)
-				if !podeColocar {
-					canPlace = canPlace && false
-					break
-				}
-				backup.itens[x][y+j] = i
+				c.itens[x][y+j] = item.Novo(id, tipo)
 				break
 			}
 		}
-
-		if canPlace {
-			for j := 0; j < int(tipo); j++ {
-				switch dir {
-				case Cima:
-					c.itens[x-j][y] = i
-					break
-				case Baixo:
-					c.itens[x+j][y] = i
-					break
-				case Esquerda:
-					c.itens[x][y-j] = i
-					break
-				default:
-					c.itens[x][y+j] = i
-					break
-				}
-			}
-		} else {
-			return false
-		}
-
 	}
 
 	return true
